83e68de6070dd22e3a3ac27b0d88418e083e3886
weepingjessica <jessicacristinarp@gmail.com>
2017-07-27 20:12:52 -0400

    cambios html local

--- blog/views.py
+++ blog/views.py
@@ -10,6 +10,7 @@ def post_list(request):
 	
 
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	print (posts)
 	return render(request,'blog/post_list.html',{'posts':posts})
 
 
