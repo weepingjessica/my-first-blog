1ccd1088410261d1db6642225f15f5877f0706f5
weepingjessica <jessicacristinarp@gmail.com>
2017-08-21 16:29:39 -0400

    new views

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 
 # Create your views here.
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post 
 
@@ -13,7 +13,9 @@ def post_list(request):
 	print (posts)
 	return render(request,'blog/post_list.html',{'posts':posts})
 
-
+def post_detail (request,pk):
+	post = get_object_or_404(Post, pk=pk)
+	return render (request, 'blog/post_detail.html', {'post': post})
 
 
 
